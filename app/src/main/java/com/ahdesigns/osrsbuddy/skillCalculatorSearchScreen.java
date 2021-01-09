a369bd01280dd192ccd0811bfbe2aafd8c71f843
Tony <anthony.hancock85@gmail.com>
2021-01-09 19:41:15 +0000

    Added the screens for the Skill Calculator.

---
+++ app/src/main/java/com/ahdesigns/osrsbuddy/skillCalculatorSearchScreen.java
@@ -0,0 +1,13 @@
+package com.ahdesigns.osrsbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class skillCalculatorSearchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.skillcalculatorsearchscreen);
+    }
+}
